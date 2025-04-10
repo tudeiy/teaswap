127ede24d51bb9541510d681dda4f7bfbd5c02b0
Tomyoka <117135910+tudeiy@users.noreply.github.com>
2025-04-10 14:59:14 +0800

    Update teaswap.sh

--- teaswap.sh
+++ teaswap.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+clear
+echo ""
+echo "                      .^!!^."
+echo "                  .:~7?7!7??7~:."
+echo "               :^!77!~:..^^~7?J?!^."
+echo "           .^!7??!^..  ..^^^^^~JJJJ7~:."
+echo "           7?????: ...^!7?!^^^~JJJJJJJ?."
+echo "           7?????:...^???J7^^^~JJJJJJJJ."
+echo "           7?????:...^??7?7^^^~JJJJJJJ?."
+echo "           7?????:...^~:.^~^^^~JJJJJJJ?."
+echo "           7?????:.. .:^!7!~^^~7?JJJJJ?."
+echo "           7?????:.:~JGP5YJJ?7!^^~7?JJ?."
+echo "           7?7?JY??JJ5BBBBG5YJJ?7!~7JJ?."
+echo "           7Y5GBBYJJJ5BBBBBBBGP5Y5PGP5J."
+echo "           ^?PBBBP555PBBBBBBBBBBBB#BPJ~"
+echo "              :!YGB#BBBBBBBBBBBBGY7^"
+echo "                 .~?5BBBBBBBBPJ~."
+echo "                     :!YGGY7:"
+echo "                        .."
+echo ""
+echo " 🚀 join channel Airdrop Sambil Rebahan : https://t.me/kingfeeder "
+echo ""
+
 echo "🚀 Setup lingkungan untuk TeaSwap"
 
 # 1. Update system
